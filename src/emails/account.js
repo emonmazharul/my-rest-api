ee75ee66cd1ad9e28d0b3193960a0d73f79e405c
Mazharul Emon <emonmazharul@yandex.com>
2019-09-25 21:09:00 +0600

    some change before deplying to heroku

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,7 @@
 const sgMail = require('@sendgrid/mail');
 
 
-sgMail.setApiKey(process.env.SENDGRIDAPIKEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
 const sendWelcomeEmail =  async (email,name) => {
