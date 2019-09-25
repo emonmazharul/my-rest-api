c087da441925766f3083e11e24d691e5a773d146
Mazharul Emon <emonmazharul@yandex.com>
2019-09-25 20:58:47 +0600

    some change before deplying to heroku

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,7 @@
 const sgMail = require('@sendgrid/mail');
 
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRIDAPIKEY);
 
 
 const sendWelcomeEmail =  async (email,name) => {
--- src/router/userRoute.js
+++ src/router/userRoute.js
@@ -30,18 +30,6 @@ router.post('/users/login', async (req,res) => {
     }
 })
 
-// router.post('/users/logout',auth, async (req,res) => {
-//   try {
-//       req.user.tokens = req.user.tokens.filter( (token) => token.token !== req.token);
-//       await req.user.save();
-//       res.send();
-
-//   } catch (e) {
-//       res.status(500).send();
-
-//   } 
-// })
-
 router.post('/users/logout',auth,async (req,res) => {
     try{
         req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
